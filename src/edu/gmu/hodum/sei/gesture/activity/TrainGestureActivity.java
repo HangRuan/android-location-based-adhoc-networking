0824b8607744ea391158f5813478b19a98d1a582
keith.h.pham@gmail.com <keith.h.pham@gmail.com@9a2a91c3-67d1-7d51-c006-9d9879d0de5c>
2012-06-29 01:28:57 +0000

    SEI Gesture Client: When training gestures, pressing the back button will save the gestures. Minor UI adjustments

--- src/edu/gmu/hodum/sei/gesture/activity/TrainGestureActivity.java
+++ src/edu/gmu/hodum/sei/gesture/activity/TrainGestureActivity.java
@@ -22,42 +22,60 @@ public class TrainGestureActivity extends Activity{
 
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.train);
-		
+
 		TextView label = (TextView) this.findViewById(R.id.label);
-		
+
 		TextView instructions = (TextView) this.findViewById(R.id.instructions);
 
 		Bundle bundle = getIntent().getExtras();
 		gestureId = bundle.getInt(TrainGestureListActivity.GESTURE_ID);
 		gesturePath = bundle.getString(TrainGestureListActivity.GESTURE_PATH);
-		
+
 		GestureRecognizerService.resetGestureCount();
 		if(gesturePath.equals(GestureRecognizerService.PATH_MAIN)){
 			GestureRecognizerService.setState(RecognizerState.LEARNING_MAIN_DEACTIVATED);
 			gestureName = GestureRecognizerService.GESTURE_NAMES_MAIN[gestureId];
-			instructions.setText("Shake three times to start training.");
+			instructions.setText("Shake three times to start training. Hold still to finish gesture.");
 		}
 		else{
 			GestureRecognizerService.setState(RecognizerState.LEARNING_CHOICE_DEACTIVATED);
 			gestureName = GestureRecognizerService.GESTURE_NAMES_CHOICE[gestureId];
-			instructions.setText("Shake once to start training.");
+			instructions.setText("Shake once to start training. Hold still to finish gesture.");
 		}
 		label.setText(gestureName);
 	}
 
 	public void btnDone_onClick(View view){
-		
+
 		//saves the gesture to file, and closes the activity
-		
+
 		GestureRecognizerService.finalizeLearning();
 		GestureRecognizerService.setPath(gesturePath);
 		GestureRecognizerService.saveGesture(gestureName);
 		GestureRecognizerService.resetGestures();
 		this.setResult(Activity.RESULT_OK);
-		
+
 		finish();
 	}
-	
-	
-	
+	public void btnCancel_onClick(View view){
+
+		//closes the activity
+
+		finish();
+	}
+
+	public void onBackPressed(){
+		//saves the gesture to file, and closes the activity
+
+		GestureRecognizerService.finalizeLearning();
+		GestureRecognizerService.setPath(gesturePath);
+		GestureRecognizerService.saveGesture(gestureName);
+		GestureRecognizerService.resetGestures();
+		this.setResult(Activity.RESULT_OK);
+
+		finish();
+	}
+
+
+
 }
